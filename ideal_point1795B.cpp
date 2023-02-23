397a19094ae9e021861734623cc94db8b08bb2d2
Akash Shridharan <akashshridharan2802@gmail.com>
2023-02-23 21:22:24 +0530

    practice

---
+++ ideal_point1795B.cpp
@@ -0,0 +1,136 @@
+// CP template in cpp by Akash Shridharan
+
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <sys/resource.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace chrono;
+// using namespace __gnu_pbds;
+
+// def
+//  #define AkashShridharan 1
+
+// Speed  ---> write "Code By AK" to take this piece of code
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define AK cout.tie(NULL);
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// Constants
+const lld pi = 3.141592653589793238;
+const ll INF = LONG_LONG_MAX;
+const ll mod = 1e9 + 7;
+
+// TypeDEF
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define rl(i, m, n) for (int i = n; i >= m; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n"; // print minus 1
+#define pn cout << "NO\n";
+#define vr(v) v.begin(), v.end()
+#define rv(v) v.end(), v.begin()
+
+// Debug
+#ifdef AkashShridharan
+#define debug(x)       \
+    cerr << #x << " "; \
+    cerr << x << " ";  \
+    cerr << endl;
+#else
+#define debug(x) ;
+#endif
+
+// Operator overloads
+
+// Sorting  the elements in a pair according to the second element
+bool sorta(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+bool sortd(const pair<int, int> &a, const pair<int, int> &b) { return (a.second > b.second); }
+
+// Code
+int ak()
+{ // referred from binarymagic utube
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int, int>> v(n);
+    map<int, int> m;
+    int countOfMaxo = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        for (int j = v[i].first; j <= v[i].second; j++)
+        {
+            m[j]++;
+        }
+    }
+    // cout<<"k frequency is "<<m[k]<<endl;
+    bool flag = true;
+    for (int i = 1; i <= 50; i++)
+    {
+        if (i != k && m[i] >= m[k])
+        {
+            flag = false;
+            int count = 0;
+            for (auto x : v)
+            {
+                if (x.second >= i && x.first <= i && (x.second < k || x.first > k))
+                {
+                    count++;
+                }
+            }
+            if (count >= (m[i] - m[k] + 1))
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if (flag == true)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
+
+// Main
+int main()
+{
+    Code By AK // Speed is applied here...
+        ll t;
+    cin >> t;
+    while (t--)
+    {
+        ak();
+    }
+    // ak();
+    // fl(i,t) {//Kickstart
+    //     cout<<"Case #"<<i+1<<": ";
+    //     ak();
+    //     cout<<"\n";
+    return 0;
+}
