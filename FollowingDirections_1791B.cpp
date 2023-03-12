ea0f2c361f01547e25321a4c8de3a97940ed7eed
Akash Shridharan <akashshridharan2802@gmail.com>
2023-03-12 10:20:57 +0530

    practice

---
+++ FollowingDirections_1791B.cpp
@@ -0,0 +1,133 @@
+
+// CP template in cpp by Akash Shridharan
+
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <sys/resource.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace chrono;
+// using namespace __gnu_pbds;
+
+// def
+//  #define AkashShridharan 1
+
+// Speed  ---> write "Code By AK" to take this piece of code
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define AK cout.tie(NULL);
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// Constants
+const lld pi = 3.141592653589793238;
+const ll INF = LONG_LONG_MAX;
+const ll mod = 1e9 + 7;
+
+// TypeDEF
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define rl(i, m, n) for (int i = n; i >= m; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n"; // print minus 1
+#define pn cout << "NO\n";
+#define vr(v) v.begin(), v.end()
+#define rv(v) v.end(), v.begin()
+
+// Debug
+#ifdef AkashShridharan
+#define debug(x)       \
+    cerr << #x << " "; \
+    cerr << x << " ";  \
+    cerr << endl;
+#else
+#define debug(x) ;
+#endif
+
+// Operator overloads
+
+// Sorting  the elements in a pair according to the second element
+bool sorta(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+bool sortd(const pair<int, int> &a, const pair<int, int> &b) { return (a.second > b.second); }
+
+// Code
+int ak()
+{ // referred from binarymagic utube
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    vector<vll> v(50, vector<ll>(50, 0));
+    v[0][0] = 1;
+    ll row = 0, col = 0;
+    fl(i, s.size())
+    {
+        char c = s[i];
+        if (s[i] == 'L')
+        {
+            row--;
+        }
+        else if (s[i] == 'R')
+        {
+            row++;
+        }
+        else if (c == 'U')
+        {
+            col++;
+        }
+        else
+        {
+            col--;
+        }
+        if (row >= 0 && row < 50 && col >= 0 && col < 50)
+        {
+            v[row][col] = 1;
+        }
+    }
+
+    if (v[1][1] == 1)
+    {
+        cout << "YES"
+             << "\n";
+    }
+    else
+    {
+        cout << "NO"
+             << "\n";
+    }
+    return 0;
+}
+
+// Main
+int main()
+{
+    Code By AK // Speed is applied here...
+        ll t;
+    cin >> t;
+    while (t--)
+    {
+        ak();
+    }
+    // ak();
+    // fl(i,t) {//Kickstart
+    //     cout<<"Case #"<<i+1<<": ";
+    //     ak();
+    //     cout<<"\n";
+    return 0;
+}
