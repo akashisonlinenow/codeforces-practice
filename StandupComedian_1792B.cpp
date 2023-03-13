9c018f4d6717f3bf2f48fd8eb3c043d6df5a90c3
Akash Shridharan <akashshridharan2802@gmail.com>
2023-03-13 23:34:01 +0530

    practice

---
+++ StandupComedian_1792B.cpp
@@ -0,0 +1,32 @@
+/*
+    binarymagic utbe
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a == 0)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+    int ans = a;
+    ans += 2 * min(b, c);
+    ans += min(a + 1, abs(c - b) + d);
+    cout << ans << endl;
+    return 0;
+}
+int main()
+{
+    int testCase = 1;
+    cin >> testCase;
+    while (testCase--)
+    {
+        solve();
+    }
+    return 0;
+}
