f8f12a82663d4908d31f280b18db75db558204ae
Akash Shridharan <akashshridharan2802@gmail.com>
2023-03-19 19:10:08 +0530

    practice

---
+++ Premutation_1790C.cpp
@@ -0,0 +1,134 @@
+// CP template in cpp by Akash Shridharan
+
+#include <iostream>
+#include <bits/stdc++.h>
+// #include <sys/resource.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace chrono;
+// using namespace __gnu_pbds;
+
+// def
+//  #define AkashShridharan 1
+
+// Speed  ---> write "Code By AK" to take this piece of code
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define AK cout.tie(NULL);
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// Constants
+const lld pi = 3.141592653589793238;
+const ll INF = LONG_LONG_MAX;
+const ll mod = 1e9 + 7;
+
+// TypeDEF
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll, ll> umll;
+typedef map<ll, ll> mll;
+
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define rl(i, m, n) for (int i = n; i >= m; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n"; // print minus 1
+#define pn cout << "NO\n";
+#define vr(v) v.begin(), v.end()
+#define rv(v) v.end(), v.begin()
+
+// Debug
+#ifdef AkashShridharan
+#define debug(x)       \
+    cerr << #x << " "; \
+    cerr << x << " ";  \
+    cerr << endl;
+#else
+#define debug(x) ;
+#endif
+
+// Operator overloads
+
+// Sorting  the elements in a pair according to the second element
+bool sorta(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+bool sortd(const pair<int, int> &a, const pair<int, int> &b) { return (a.second > b.second); }
+
+// Code
+void ak()
+{ // refer peter griffin utube...
+    ll n;
+    cin >> n; // idea - obtain max freq of no. at (i,0) ; i->0 to n-1 that no. will come first in o/p and print all the no.s of the row in sequence where the first element is not the max. freq wala element
+
+    int brr[n][n - 1];
+    fl(i, n)
+    {
+        fl(j, n - 1)
+        {
+            cin >> brr[i][j];
+        }
+    }
+
+    int freq[n + 1] = {0};
+    fl(i, n)
+    {
+        freq[brr[i][0]]++;
+    }
+
+    int ans = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        if (freq[i] == n - 1)
+        {
+            ans = i;
+            break;
+        }
+    }
+
+    cout << ans << " ";
+
+    int row = 0;
+    fl(i, n)
+    {
+        if (brr[i][0] != ans)
+        {
+            row = i;
+            break;
+        }
+    }
+
+    fl(i, n - 1)
+    {
+        cout << brr[row][i] << " ";
+    }
+
+    cout << "\n";
+}
+
+// Main
+int main()
+{
+    Code By AK // Speed is applied here...
+        ll t;
+    cin >> t;
+    while (t--)
+    {
+        ak();
+    }
+    // ak();
+    // fl(i,t) {//Kickstart
+    //     cout<<"Case #"<<i+1<<": ";
+    //     ak();
+    //     cout<<"\n";
+    return 0;
+}
